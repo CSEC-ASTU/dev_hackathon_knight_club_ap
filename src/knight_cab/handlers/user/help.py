2a2954757f8f2e903ec34bfc5c567adfba697db7
Biruk Alamirew <biruk.alamirew77@gmail.com>
2022-05-08 12:10:01 +0300

    fixed import issue

--- src/knight_cab/handlers/user/help.py
+++ src/knight_cab/handlers/user/help.py
@@ -1,9 +1,9 @@
 from aiogram import types
 
-from utils.misc import rate_limit
+# from utils.misc import rate_limit
 
 
-@rate_limit(5, "help")
+# @rate_limit(5, "help")
 async def bot_help(msg: types.Message):
     text = ["Help Section: ", "/start - Main Menu", "/help - Help Section"]
     await msg.answer("\n".join(text))
